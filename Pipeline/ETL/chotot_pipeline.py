220934718c7dfc5992e6f27d1fef8a4ab89febf4
baopdg <baopdg@d-soft.com.vn>
2023-02-08 17:31:54 +0700

    Update pipeline for factory

---
+++ Pipeline/ETL/chotot_pipeline.py
@@ -0,0 +1,70 @@
+import mysql.connector
+import json
+import os
+from os import path
+from urllib.parse import urlparse
+import requests
+
+bifm_mysql = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    user="root",
+    password="root",
+    database="bifm",
+    charset="utf8mb4"
+)
+
+bifm_cursor = bifm_mysql.cursor()
+bifm_cursor.execute("SELECT * FROM chotot_crawler")
+chotot_data_all = bifm_cursor.fetchall()
+
+for chotot_data in chotot_data_all:
+    try:
+        data_crawler = json.loads(chotot_data[1])
+            
+        item_id = data_crawler['ad']['ad_id']
+        name = data_crawler['ad']['subject']
+        price = data_crawler['ad']['price'] if 'price' in data_crawler['ad'] else 0
+        description = data_crawler['ad']['body']
+        category_id = data_crawler['ad']['category']
+        images = data_crawler['ad']['images'] if 'images' in data_crawler['ad'] else None
+        
+        bifm_cursor.execute("SELECT * FROM product_chotot where item_id = " + str(item_id))
+        check_data = bifm_cursor.fetchall()
+        
+        if (len(check_data) == 0):
+            val = (item_id, name, price, description, category_id)
+            bifm_cursor.execute("INSERT INTO product_chotot (item_id,name,price,description,category_id) VALUES (%s,%s,%s,%s,%s)", val)
+            bifm_mysql.commit()
+
+            print("Insert info chotot: " + str(item_id))
+            
+            if images is not None:
+                for thumbnail_url in images:
+                    thumbnail_image = os.path.basename(urlparse(thumbnail_url).path)
+                    product_id = bifm_cursor.lastrowid
+                    image_path = 'C:/DSOFT/DATA/Chotot/' + thumbnail_image
+                    
+                    if path.exists(image_path) is False:
+                        with open(image_path, 'wb') as handle:
+                            response = requests.get(thumbnail_url, stream=True)
+
+                            if not response.ok:
+                                print(response)
+
+                            for block in response.iter_content(1024):
+                                if not block:
+                                    break
+
+                                handle.write(block)
+                    
+                    val = (thumbnail_url, thumbnail_image, product_id)
+                    bifm_cursor.execute("INSERT INTO img_chotot (thumbnail_url,thumbnail_image,product_id) VALUES (%s,%s,%s)", val)
+                    bifm_mysql.commit()
+                    
+                    print("Insert image chotot: " + str(bifm_cursor.lastrowid))
+        else:
+            print("Exist info chotot: " + str(item_id))
+    except print(chotot_data):
+        print(data_crawler)
+        # pass
--- Pipeline/ETL/shopee_pipeline.js
+++ Pipeline/ETL/shopee_pipeline.js
@@ -1,16 +1,18 @@
-import * as puppeteer from "puppeteer";
-import * as fs from "fs";
 import { MongoClient } from "mongodb";
 import * as mysql from "mysql";
+import * as fs from "fs";
+import request from "request";
 
 (async () => {
+
     const data_crawler = await new Promise(async (resolve) => {
         MongoClient.connect("mongodb://127.0.0.1:27017/", function (err, db) {
             if (err) throw err;
             var dbo = db.db("shopee");
             dbo
-                .collection("product_list")
+                .collection("product_list4")
                 .find({})
+                // .limit(1)
                 .toArray(function (err, result) {
                     if (err) throw err;
                     resolve(result);
@@ -19,24 +21,22 @@ import * as mysql from "mysql";
         });
     });
 
-
     let product_mysql = [];
-    for (var data in data_crawler) {
-        for (var item in data_crawler[data].items) {
-            const product_mongo = data_crawler[data]?.items[item]?.item_basic;
+    for (var index in data_crawler) {
+        for (var item in data_crawler[index].data.sections[0].data.item) {
+            const product_mongo = data_crawler[index].data.sections[0].data.item[item];
             product_mysql.push([
                 product_mongo?.name,
                 product_mongo?.price,
                 null,
                 product_mongo?.itemid,
                 product_mongo?.shopid,
-                product_mongo?.catid,
+                data_crawler[index].catid,
                 null,
+                product_mongo?.images
             ]);
         }
     }
-
-    // console.log(product_mysql[0]);
 
     var con = mysql.createConnection({
         host: "localhost",
@@ -46,20 +46,75 @@ import * as mysql from "mysql";
         charset: 'utf8mb4'
     });
 
+    var sqlInfo = "INSERT INTO product_shopee2 (name, price, description, item_id, shop_id, category_id, review_id) VALUES ?";
+    var sqlImg = "INSERT INTO product_img2 (thumbnail_url, thumbnail_image, product_id) VALUES ?";
 
-    var sql = "INSERT INTO productshopee (name, price, description, item_id, shop_id, category_id, review_id) VALUES ?";
+    var img_mysql = [];
 
-    con.connect(function (err) {
+    con.connect(async function (err) {
         if (err) throw err;
         console.log("Connected!");
         for (var key in product_mysql) {
-    
-          con.query(sql, [[product_mysql[key]]], function (err, result) {
-            if (err) throw err;
-            console.log("Number of records inserted: " + result.affectedRows);
-          });
-    
+            let itemId = product_mysql[key][3];
+            const product_check = await new Promise(async (resolve) => {
+                con.query("SELECT * FROM product_shopee2 WHERE item_id = " + itemId + " LIMIT 1", function (err, result, fields) {
+                    if (err) throw err;
+                    if (result.length > 0) {
+                        console.log("Exist product: " + itemId);
+                    }
+                    resolve(result);
+                });
+            });
+
+            if (product_check.length == 0) {
+
+                var image_arr = product_mysql[key][7];
+                product_mysql[key].splice(7, 1);
+
+
+                const id = await new Promise(function (resolve) {
+                    con.query(sqlInfo, [[product_mysql[key]]], function (err, result) {
+                        if (err) throw err;
+
+                        console.log("Inserted info: " + result.insertId);
+                        resolve(result.insertId);
+                    });
+                });
+
+                for (var key in image_arr) {
+                    let url = 'https://cf.shopee.vn/file/' + image_arr[key];
+                    let img_path = 'C:/DSOFT/DATA/Shoppe/' + image_arr[key] + '.jpg';
+                    img_mysql = [
+                        url,
+                        image_arr[key],
+                        id
+                    ];
+
+
+                    if (!fs.existsSync(img_path)) {
+
+                        await new Promise(async function (resolve) {
+                            request.head(url, function (err, res, body) {
+                                //   console.log('content-type:', res.headers['content-type']);
+                                //   console.log('content-length:', res.headers['content-length']);
+                                request(url).pipe(fs.createWriteStream(img_path)).on('close', function () {
+                                    console.log('Download image: ' + url);
+                                });
+                                resolve();
+                            });
+                        });
+                    }
+
+                    con.query(sqlImg, [[img_mysql]], function (err, result) {
+                        if (err) throw err;
+
+                        console.log("Inserted image: " + result.insertId);
+                    });
+                }
+            }
+
         }
         con.end();
     });
+
 })();
