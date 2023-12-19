086180481715ae0199a815e16796959704d5fff5
baopdg <baopdg@d-soft.com.vn>
2023-12-19 13:46:10 +0700

    Update pipeline config spear

---
+++ Pipeline/High_performance/scala_spear/src/main/scala/Main.scala
@@ -0,0 +1,31 @@
+import com.github.edge.roman.spear.SpearConnector
+import org.apache.spark.sql.SaveMode
+
+object Main {
+  def main(args: Array[String]) {
+    val targetProps = Map(
+        "driver" -> "org.postgresql.Driver",
+        "user" -> "postgres_user",
+        "password" -> "mysecretpassword",
+        "url" -> "jdbc:postgresql://postgres:5432/pgdb"
+      )
+
+    val csvJdbcConnector = SpearConnector
+        .createConnector(name="CSVtoPostgresConnector")
+        .source(sourceType = "file", sourceFormat = "csv")
+        .target(targetType = "relational", targetFormat = "jdbc")
+        .getConnector   
+    
+    csvJdbcConnector.setVeboseLogging(true)
+    csvJdbcConnector
+      .source(sourceObject="file:///opt/spear-framework/data/us-election-2012-results-by-county.csv", Map("header" -> "true", "inferSchema" -> "true"))
+      .saveAs("__tmp__")
+      .transformSql(
+        """select state_code,party,
+          |sum(votes) as total_votes
+          |from __tmp__
+          |group by state_code,party""".stripMargin)
+      .targetJDBC(objectName="mytable", props=targetProps, saveMode=SaveMode.Overwrite)
+    csvJdbcConnector.stop()
+  }
+}
